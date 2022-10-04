539d3e3175e61d743bc4b0b41ad2a14cd50c91c2
Antoine RICHARD <antoine.richard.partner@decathlon.com>
2022-10-05 00:33:05 +0200

    --wip-- [skip ci]

---
+++ packages/workshop-05/__solution__/AppOnMount.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Header from '../../common/components/Header.svelte';
+	import PersonCard from '../../common/components/person-card/PersonCard.svelte';
+
+	import { loadPeople } from './../../common/utils';
+
+	let people;
+
+	onMount(async () => {
+		people = await loadPeople();
+	});
+</script>
+
+<Header />
+
+<section class="section">
+	{#if people}
+		<div class="columns is-multiline margin-medium mt-4">
+			{#each people as person}
+				<div class="column is-4">
+					<PersonCard {person} />
+				</div>
+			{/each}
+		</div>
+	{:else}
+		loading...
+	{/if}
+</section>
---
+++ packages/workshop-05/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Header from '../../common/components/Header.svelte';
+	import PersonCard from '../../common/components/person-card/PersonCard.svelte';
+
+	import { people } from '../../../data/people.json';
+</script>
+
+<Header />
+
+<section class="section">
+	<div class="columns is-multiline margin-medium mt-4">
+		{#each people as person}
+			<div class="column is-4">
+				<PersonCard {person} />
+			</div>
+		{/each}
+	</div>
+</section>
