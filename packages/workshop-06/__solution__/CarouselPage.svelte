965d6274721c4d413e16eaf161ee321db32c1e1b
Romain Torond <romain.torond@gmail.com>
2022-10-14 13:49:50 +0200

    add workshop 6

---
+++ packages/workshop-06/__solution__/CarouselPage.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import PeopleCarousel from "$common/components/people-carousel/PeopleCarousel.svelte";
+  import { people } from "./people.store";
+</script>
+
+<PeopleCarousel people={$people} />
---
+++ packages/workshop-06/src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Header from '$common/components/header/Header.svelte';
+	import HeaderMenu from '$common/components/header/HeaderMenu.svelte';
+	import Loader from '$common/components/Loader.svelte';
+	import { loadPeople } from '$common/utils';
+
+	import { isPeopleLoading } from './people.store';
+
+	let currentPage: 'LIST' | 'CAROUSEL' = 'LIST';
+
+	const switchPage = (event) => (currentPage = event.detail);
+
+	onMount(async () => {
+		// loadPeople();
+	});
+
+	$: CurrentView = null;
+</script>
+
+<Header>
+	<HeaderMenu slot="menu" {currentPage} on:clickLink={switchPage} />
+</Header>
+
+<div class="container pt-6">
+	{#if isPeopleLoading}
+		<Loader class="mt-6" />
+	{:else}
+		<p>
+			Update ListPage & CarouselPage to use people store, and use "svelte:component" special element
+			to switch between with "CurrentView"
+			<br />
+			<br />
+			Note: ListPage & CarouselPage should have props, use people store instead in them.
+		</p>
+	{/if}
+</div>
